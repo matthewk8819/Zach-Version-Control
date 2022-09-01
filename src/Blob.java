52efa4b0dd2290faaf6bc1966da1c0498ead6ed6
Zach Rossen <zachary.rossen@gmail.com>
2022-09-01 10:37:10 -0700

    Added in SHA1 method

--- src/Blob.java
+++ src/Blob.java
@@ -1,5 +1,59 @@
 
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Blob {
 
+	private String SHA1;
+	
+	public Blob(String fileName) {
+		SHA1 = encryptThisString(fileName);
+		
+		File file = new File(SHA1);	
+	}
+	
+	private static String getFileString(String fileName) throws IOException{
+		Path filePath = Path.of(fileName);
+		
+		return Files.readString(filePath);
+	}
+	
+	private String encryptThisString(String input)
+    {
+        try {
+            // getInstance() method is called with algorithm SHA-1
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+ 
+            // digest() method is called
+            // to calculate message digest of the input string
+            // returned as array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+ 
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+ 
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+ 
+            // Add preceding 0s to make it 32 bit
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+ 
+            // return the HashText
+            return hashtext;
+        }
+ 
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+	
 	
 }
