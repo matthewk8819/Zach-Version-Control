9bf0976a91acfb1da94f60ebac3d51f4411fbef5
Zach Rossen <zachary.rossen@gmail.com>
2022-09-01 10:07:12 -0700

    Create basic tester

--- src/BlobTester.java
+++ src/BlobTester.java
@@ -1,9 +1,11 @@
 
+import java.io.IOException;
+
 public class BlobTester {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		Blob blob = new Blob(".\\test\\something.txt");
 	}
 
 }
