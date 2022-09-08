5085da3a045df3232c8208c9923c9fef0c8238ac
Zach Rossen <zachary.rossen@gmail.com>
2022-09-08 12:15:43 -0700

    Creating Index

--- src/Index.java
+++ src/Index.java
@@ -1,5 +1,23 @@
+import java.io.File;
 
 public class Index {
 
+	//creates index file
+	private File index;
 	
+	//constructor
+	public Index() {
+		//initializes index file
+		index = new File("index");
+	}
+	
+	//creates and adds new blob to index and objects folder
+	public void add(String fileName) {
+		
+	}
+	
+	//removes blob from index and objects folder
+	public void remove(String fileName) {
+		
+	}
 }
